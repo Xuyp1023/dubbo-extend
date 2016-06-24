d7fa476a3aee9c63119a938119725e6db9d32d82
Hewei <hewei@bytter.com>
2016-06-24 08:51:08 +0800

    增加取值为空处理

--- src/main/java/com/betterjr/dubbo/extend/filter/user/UserComsumerFilter.java
+++ src/main/java/com/betterjr/dubbo/extend/filter/user/UserComsumerFilter.java
@@ -20,14 +20,16 @@ public class UserComsumerFilter implements Filter{
 	Logger logger=LoggerFactory.getLogger(UserComsumerFilter.class);
 	public Result invoke(Invoker<?> invoker, Invocation invocation) throws RpcException {
 		// TODO Auto-generated method stub
-		Subject subject = SecurityUtils.getSubject();  
-		String sessionid=(String) subject.getSession().getId();
 		logger.debug("UserComsumerFilter test :"+invocation.toString());
 		logger.debug("UserComsumerFilter test :"+invoker.getUrl());
-		logger.debug("UserComsumerFilter test :sessionid="+sessionid);
-		invocation.getAttachments().put(FilterConstants.FilterAttachmentSessionId, sessionid);
-		logger.debug("UserComsumerFilter test :"+invocation.toString());
-		logger.debug("UserComsumerFilter test invoker interface:"+invoker.getInterface().getName());
+		Subject subject = SecurityUtils.getSubject();  
+		if(subject!=null && subject.getSession()!=null){
+			String sessionid=(String) subject.getSession().getId();
+			logger.debug("UserComsumerFilter test :sessionid="+sessionid);
+			invocation.getAttachments().put(FilterConstants.FilterAttachmentSessionId, sessionid);
+			logger.debug("UserComsumerFilter test :"+invocation.toString());
+			logger.debug("UserComsumerFilter test invoker interface:"+invoker.getInterface().getName());
+		}
 		return invoker.invoke(invocation);
 	}
 
--- src/main/java/com/betterjr/dubbo/extend/filter/user/UserProviderFilter.java
+++ src/main/java/com/betterjr/dubbo/extend/filter/user/UserProviderFilter.java
@@ -1,5 +1,11 @@
 package com.betterjr.dubbo.extend.filter.user;
 
+import org.apache.shiro.authc.UsernamePasswordToken;
+import org.apache.shiro.session.Session;
+import org.apache.shiro.subject.SimplePrincipalCollection;
+import org.apache.shiro.subject.Subject;
+import org.apache.shiro.subject.support.DefaultSubjectContext;
+
 import com.alibaba.dubbo.common.Constants;
 import com.alibaba.dubbo.common.extension.Activate;
 import com.alibaba.dubbo.common.logger.Logger;
@@ -9,19 +15,42 @@ import com.alibaba.dubbo.rpc.Invocation;
 import com.alibaba.dubbo.rpc.Invoker;
 import com.alibaba.dubbo.rpc.Result;
 import com.alibaba.dubbo.rpc.RpcException;
+import com.betterjr.common.security.shiro.session.RedisSessionDAO;
 import com.betterjr.common.utils.StaticThreadLocal;
 
 @Activate(group={Constants.PROVIDER})
 public class UserProviderFilter implements Filter {
 	Logger logger=LoggerFactory.getLogger(UserProviderFilter.class);
+	
+	private RedisSessionDAO redisSessionDAO;
+	
+	public void setRedisSessionDAO(RedisSessionDAO redisSessionDAO) {
+		this.redisSessionDAO = redisSessionDAO;
+	}
+
 	public Result invoke(Invoker<?> invoker, Invocation invocation) throws RpcException {
 		// TODO Auto-generated method stub
 		logger.debug("UserProviderFilter test :"+invocation.toString());
 		logger.debug("UserProviderFilter test :"+invoker.getUrl());
-		String user=invocation.getAttachment(FilterConstants.FilterAttachmentSessionId);
-		logger.debug("UserProviderFilter test :sessionid="+user);
-		logger.debug("UserProviderFilter test :invoker interface="+invoker.getInterface().getName());
-		StaticThreadLocal.storeSessionId(user);
+		String sessionId=invocation.getAttachment(FilterConstants.FilterAttachmentSessionId);
+		if(sessionId!=null){
+			logger.debug("UserProviderFilter test :sessionid="+sessionId);
+			logger.debug("UserProviderFilter test :invoker interface="+invoker.getInterface().getName());
+			StaticThreadLocal.storeSessionId(sessionId);
+			
+			Session session=redisSessionDAO.doReadSession(sessionId);
+			if(session !=null){
+				for(Object obj:session.getAttributeKeys()){
+					logger.debug("UserProviderFilter test ,session attrs:"+session.getAttribute(obj));
+				}
+				StaticThreadLocal.storeSession(session);
+				
+				SimplePrincipalCollection col=(SimplePrincipalCollection)session.getAttribute(DefaultSubjectContext.PRINCIPALS_SESSION_KEY);
+				if(col!=null){
+					StaticThreadLocal.storePrincipal(col.getPrimaryPrincipal());
+				}
+			}
+		}
 		return invoker.invoke(invocation);
 	}
 
