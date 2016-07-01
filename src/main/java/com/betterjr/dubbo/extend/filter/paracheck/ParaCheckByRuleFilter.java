42a609cfc4540b4bbd373d3d65a8d04f93c7047e
Hewei <hewei@bytter.com>
2016-07-01 13:19:46 +0800

    增加功能：将入参写入缓存

--- src/main/java/com/betterjr/dubbo/extend/filter/paracheck/ParaCheckByRuleFilter.java
+++ src/main/java/com/betterjr/dubbo/extend/filter/paracheck/ParaCheckByRuleFilter.java
@@ -1,5 +1,10 @@
 package com.betterjr.dubbo.extend.filter.paracheck;
 
+import java.lang.reflect.Method;
+import java.lang.reflect.Parameter;
+import java.util.HashMap;
+import java.util.Map;
+
 import com.alibaba.dubbo.common.Constants;
 import com.alibaba.dubbo.common.extension.Activate;
 import com.alibaba.dubbo.common.logger.Logger;
@@ -9,6 +14,7 @@ import com.alibaba.dubbo.rpc.Invocation;
 import com.alibaba.dubbo.rpc.Invoker;
 import com.alibaba.dubbo.rpc.Result;
 import com.alibaba.dubbo.rpc.RpcException;
+import com.betterjr.common.utils.StaticThreadLocal;
 import com.betterjr.dubbo.extend.filter.user.UserComsumerFilter;
 import com.betterjr.modules.rule.service.RuleServiceDubboFilterInvoker;
 
@@ -25,13 +31,38 @@ public class ParaCheckByRuleFilter implements Filter{
 	public Result invoke(Invoker<?> invoker, Invocation invocation) throws RpcException {
 		// TODO Auto-generated method stub
 		try{
+			//将入参写入缓存
+			this.initMethodPara(invoker, invocation);
+			
+			//参数验证
 			logger.debug("ParaCheckByRuleFilter test:invoker:"+this.ruleServiceDubboFilterInvoker);
 			logger.debug("ParaCheckByRuleFilter test:doing paras checking.");
-			return this.ruleServiceDubboFilterInvoker.doAround(invoker, invocation);
+			Result re= this.ruleServiceDubboFilterInvoker.doAround(invoker, invocation);
+			
+			return re;
 		}catch(Throwable e){
 			e.printStackTrace();
 			throw new RpcException(invocation.getMethodName()+"'s input parameters are valid.",e);
 		}
+	}
+	
+	public void initMethodPara(Invoker<?> invoker, Invocation invocation){
+		Class cls=invoker.getInterface();
+		Object[] arguValues=invocation.getArguments();
+		String methName=invocation.getMethodName();
+		try {
+			Method method=cls.getMethod(methName, invocation.getParameterTypes());
+			Map map=new HashMap();
+			int index=0;
+			for(Parameter para:method.getParameters()){
+				map.put(para.getName(), arguValues[index]);
+				index++;
+			}
+			StaticThreadLocal.storeDubboMethodParaMap(map);
+			logger.debug("ParaCheckByRuleFilter test:write method para map:"+map);
+		} catch (Exception e) {
+			logger.error("init Method paras failed! ",e);
+		} 
 	}
 
 }
