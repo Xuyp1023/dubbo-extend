b6445ffc3e2b19c929420321b7a0ff3e920658cb
zhoucy <zhoucy@bytter.com>
2017-10-16 19:18:51 +0800

    JAVA 代码使用阿里巴巴的规范

--- src/main/java/com/betterjr/dubbo/extend/filter/execption/ThrowBytterExceptionFilter.java
+++ src/main/java/com/betterjr/dubbo/extend/filter/execption/ThrowBytterExceptionFilter.java
@@ -15,20 +15,20 @@ import com.alibaba.dubbo.rpc.RpcResult;
 import com.alibaba.dubbo.rpc.service.GenericService;
 import com.betterjr.common.exception.BytterException;
 
-
-@Activate(group = Constants.PROVIDER,order = 30000)
+@Activate(group = Constants.PROVIDER, order = 30000)
 public class ThrowBytterExceptionFilter implements Filter {
 
     private final Logger logger;
-    
+
     public ThrowBytterExceptionFilter() {
         this(LoggerFactory.getLogger(ThrowBytterExceptionFilter.class));
     }
-    
+
     public ThrowBytterExceptionFilter(Logger logger) {
         this.logger = logger;
     }
-    
+
+    @Override
     public Result invoke(Invoker<?> invoker, Invocation invocation) throws RpcException {
         try {
             Result result = invoker.invoke(invocation);
@@ -40,22 +40,26 @@ public class ThrowBytterExceptionFilter implements Filter {
                     if (exception instanceof BytterException) {
                         return new RpcResult(new RpcException(exception));
                     }
-                } catch (Throwable e) {
-                    logger.warn("Fail to ThrowBytterExceptionFilter when called by " + RpcContext.getContext().getRemoteHost()
-                            + ". service: " + invoker.getInterface().getName() + ", method: " + invocation.getMethodName()
-                            + ", exception: " + e.getClass().getName() + ": " + e.getMessage(), e);
+                }
+                catch (Throwable e) {
+                    logger.warn("Fail to ThrowBytterExceptionFilter when called by "
+                            + RpcContext.getContext().getRemoteHost() + ". service: " + invoker.getInterface().getName()
+                            + ", method: " + invocation.getMethodName() + ", exception: " + e.getClass().getName()
+                            + ": " + e.getMessage(), e);
                     return result;
                 }
             }
             return result;
-        } catch (RuntimeException e) {
+        }
+        catch (RuntimeException e) {
             // 如果是bytterException异常，直接抛出
             if (e instanceof BytterException) {
                 return new RpcResult(new RpcException(e));
-            }else{
-                logger.error("Got unchecked and undeclared exception which called by " + RpcContext.getContext().getRemoteHost()
-                        + ". service: " + invoker.getInterface().getName() + ", method: " + invocation.getMethodName()
-                        + ", exception: " + e.getClass().getName() + ": " + e.getMessage(), e);
+            } else {
+                logger.error("Got unchecked and undeclared exception which called by "
+                        + RpcContext.getContext().getRemoteHost() + ". service: " + invoker.getInterface().getName()
+                        + ", method: " + invocation.getMethodName() + ", exception: " + e.getClass().getName() + ": "
+                        + e.getMessage(), e);
                 throw e;
             }
         }
--- src/main/java/com/betterjr/dubbo/extend/filter/paracheck/ParaCheckByRuleFilter.java
+++ src/main/java/com/betterjr/dubbo/extend/filter/paracheck/ParaCheckByRuleFilter.java
@@ -19,61 +19,64 @@ import com.betterjr.common.utils.StaticThreadLocal;
 import com.betterjr.dubbo.extend.filter.user.UserConsumerFilter;
 import com.betterjr.modules.rule.service.RuleServiceDubboFilterInvoker;
 
-@Activate(group={Constants.PROVIDER},order = 40000)
-public class ParaCheckByRuleFilter implements Filter{
-	Logger logger=LoggerFactory.getLogger(UserConsumerFilter.class);
+@Activate(group = { Constants.PROVIDER }, order = 40000)
+public class ParaCheckByRuleFilter implements Filter {
+    Logger logger = LoggerFactory.getLogger(UserConsumerFilter.class);
 
-	private RuleServiceDubboFilterInvoker ruleServiceDubboFilterInvoker;
+    private RuleServiceDubboFilterInvoker ruleServiceDubboFilterInvoker;
 
-	public void setRuleServiceDubboFilterInvoker(RuleServiceDubboFilterInvoker ruleServiceDubboFilterInvoker) {
-		this.ruleServiceDubboFilterInvoker = ruleServiceDubboFilterInvoker;
-	}
+    public void setRuleServiceDubboFilterInvoker(RuleServiceDubboFilterInvoker ruleServiceDubboFilterInvoker) {
+        this.ruleServiceDubboFilterInvoker = ruleServiceDubboFilterInvoker;
+    }
 
-	public Result invoke(Invoker<?> invoker, Invocation invocation) throws RpcException {
-		// TODO Auto-generated method stub
-		try{
-		    StaticThreadLocal.clearThreadLocal();
-		    RuleServiceDubboFilterInvoker.clearThreadLocal();
-			//将入参写入缓存
-			this.initMethodPara(invoker, invocation);
-			
-			//参数验证
-			logger.debug("ParaCheckByRuleFilter test:invoker:"+this.ruleServiceDubboFilterInvoker);
-			logger.debug("ParaCheckByRuleFilter test:doing paras checking.");
-			Result re= this.ruleServiceDubboFilterInvoker.doAround(invoker, invocation);
-			
-			return re;
-		}catch(BytterException byEx){
-		    logger.error(byEx.getMessage(),byEx);
-		    throw byEx;
-		}
-		catch(RpcException ex){
-			logger.error(ex.getMessage(),ex);
-			throw ex;
-		}
-		catch(Throwable e){
-			logger.error(e.getMessage(),e);
-			throw new RpcException(e.getMessage(),e);
-		}
-	}
-	
-	public void initMethodPara(Invoker<?> invoker, Invocation invocation){
-		Class cls=invoker.getInterface();
-		Object[] arguValues=invocation.getArguments();
-		String methName=invocation.getMethodName();
-		try {
-			Method method=cls.getMethod(methName, invocation.getParameterTypes());
-			Map map=new HashMap();
-			int index=0;
-			for(Parameter para:method.getParameters()){
-				map.put(para.getName(), arguValues[index]);
-				index++;
-			}
-			StaticThreadLocal.storeDubboMethodParaMap(map);
-			logger.debug("ParaCheckByRuleFilter test:write method para map:"+map);
-		} catch (Exception e) {
-			logger.error("init Method paras failed! ",e);
-		} 
-	}
+    @Override
+    public Result invoke(Invoker<?> invoker, Invocation invocation) throws RpcException {
+        // TODO Auto-generated method stub
+        try {
+            StaticThreadLocal.clearThreadLocal();
+            RuleServiceDubboFilterInvoker.clearThreadLocal();
+            // 将入参写入缓存
+            this.initMethodPara(invoker, invocation);
+
+            // 参数验证
+            logger.debug("ParaCheckByRuleFilter test:invoker:" + this.ruleServiceDubboFilterInvoker);
+            logger.debug("ParaCheckByRuleFilter test:doing paras checking.");
+            Result re = this.ruleServiceDubboFilterInvoker.doAround(invoker, invocation);
+
+            return re;
+        }
+        catch (BytterException byEx) {
+            logger.error(byEx.getMessage(), byEx);
+            throw byEx;
+        }
+        catch (RpcException ex) {
+            logger.error(ex.getMessage(), ex);
+            throw ex;
+        }
+        catch (Throwable e) {
+            logger.error(e.getMessage(), e);
+            throw new RpcException(e.getMessage(), e);
+        }
+    }
+
+    public void initMethodPara(Invoker<?> invoker, Invocation invocation) {
+        Class cls = invoker.getInterface();
+        Object[] arguValues = invocation.getArguments();
+        String methName = invocation.getMethodName();
+        try {
+            Method method = cls.getMethod(methName, invocation.getParameterTypes());
+            Map map = new HashMap();
+            int index = 0;
+            for (Parameter para : method.getParameters()) {
+                map.put(para.getName(), arguValues[index]);
+                index++;
+            }
+            StaticThreadLocal.storeDubboMethodParaMap(map);
+            logger.debug("ParaCheckByRuleFilter test:write method para map:" + map);
+        }
+        catch (Exception e) {
+            logger.error("init Method paras failed! ", e);
+        }
+    }
 
 }
